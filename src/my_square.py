6dc52abbe95941b85b7badeb651164d84e894ed9
Nikhil Rao <gopai.nikhil@gmail.com>
2017-04-18 11:46:31 -0400

    added a new function

--- src/my_square.py
+++ src/my_square.py
@@ -4,4 +4,8 @@ def my_square(y):
     # this script requires a intiger argument
     # here we use ** operator
     return(y**2)
+
+def my_square2(x):
+    return(x*X)
 print(my_square(42))
+print(my_square2(5))
